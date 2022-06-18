fd0b0a9e56c456eed4bacb8253a3210c8dcec884
M0lotov <84855814+M0lotov@users.noreply.github.com>
2022-06-18 16:04:08 +0800

    ...

--- matrix_test.cpp
+++ matrix_test.cpp
@@ -8,14 +8,34 @@ TEST(TypeTest, Int) {
     int data[2][2]{{1, 2}, {3, 4}};
     Matrix<int> m(2, 2, (void*)data);
     ASSERT_EQ(m.get(0, 0), 1);
+    ASSERT_EQ(m.get(0, 1), 2);
+    ASSERT_EQ(m.get(1, 0), 3);
+    ASSERT_EQ(m.get(1, 1), 4);
 }
 
 TEST(TypeTest, Double) {
-
+    double data[2][2]{{1.0,2.0},{3.0,4.0}};
+    Matrix<double> m(2,2,(void*)data);
+    ASSERT_EQ(m.get(0,0), 1.0);
+    ASSERT_EQ(m.get(0,1), 2.0);
+    ASSERT_EQ(m.get(1,0), 3.0);
+    ASSERT_EQ(m.get(1,1), 4.0);
 }
 
 TEST(TypeTest, Complex) {
+    complex<int> data[1][2]{{complex<int>(1,1), complex<int>(2,2)}};
+    Matrix<complex<int>> m(1,2,(void*)data);
+    ASSERT_EQ(m.get(0,0), complex<int>(1,1));
+    ASSERT_EQ(m.get(0,1), complex<int>(2,2));
+}
 
+TEST(TypeTest, SelfDefined){
+    MyInt data1[1][2]{{MyInt(1), MyInt(2)}};
+    Matrix<MyInt> m1(1,2,(void*)data1);
+    MyInt data2[2][1]{{MyInt(1)}, {MyInt(2)}};
+    Matrix<MyInt> m2(2,1,(void*)data2);
+    Matrix<MyInt> res(1,1,(void*)(new MyInt(5)));
+    ASSERT_TRUE(res.equals(m1 * m2));
 }
 
 TEST(ArithmeticTest, addition) {
@@ -193,6 +213,54 @@ TEST(TestSuite5, company_matrix) {
                     {1, 1}};
     Matrix<int> res(2, 2, (void*)res_d);
     ASSERT_TRUE(res.equals(a.company_matrix()));
+}
+
+TEST(SliceTest, SliceTest){
+    int data[3][4]{{1,2,3,4}, {5,6,7,8}, {9,10,11,12}};
+    Matrix<int> m(3,4,(void*)data);
+    int sd1[2][2]{{1,2},{5,6}};
+    Matrix<int> s1(2,2,(void*)sd1);
+    int sd2[3][3]{{2,3,4},{6,7,8},{10,11,12}};
+    Matrix<int> s2(3,3,(void*)sd2);
+    cout << m.slice(0,1,0,1);
+    cout << m.slice(0,2,1,3);
+    ASSERT_TRUE(s1.equals(m.slice(0,1,0,1)));
+    ASSERT_TRUE(s2.equals(m.slice(0,2,1,3)));
+}
+
+TEST(ReshapeTest, ReshapeTest){
+    int data[3][4]{{1,2,3,4}, {5,6,7,8}, {9,10,11,12}};
+    Matrix<int> m(3,4,(void*)data);
+    int rd1[2][6]{{1,2,3,4,5,6},{7,8,9,10,11,12}};
+    Matrix<int> r1(2,6,(void*)rd1);
+    int rd2[4][3]{{1,2,3},{4,5,6},{7,8,9},{10,11,12}};
+    Matrix<int> r2(4,3,(void*)rd2);
+    ASSERT_TRUE(r1.equals(m.reshape(2,6)));
+    ASSERT_TRUE(r2.equals(m.reshape(4,3)));
+}
+
+TEST(ConvolutionTest, ConvolutionTest){
+    int md1[3][3]{{1,2,3},{4,5,6},{7,8,9}};  //square matrix convolution
+    int md2[2][5]{{1,2,3,4,5},{6,7,8,9,10}}; //arbitrary size
+    Matrix<int> m1(3,3,(void*)md1);
+    Matrix<int> m2(2,5,(void*)md2);
+    int resd1[5][5]{
+            {1,4,10,12,9},
+            {8,26,56,54,36},
+            {30,84,165,144,90},
+            {56,134,236,186,108},
+            {49,112,190,144,81}
+    };
+    int resd2[4][7]{
+            {1,4,10,16,22,22,15},
+            {10,32,68,89,110,96,60},
+            {31,80,149,188,227,180,105},
+            {42,97,166,190,214,161,90}
+    };
+    Matrix<int> res1(5,5,(void*)resd1);
+    Matrix<int> res2(4,7,(void*)resd2);
+    ASSERT_TRUE(res1.equals(conv(m1,m1)));
+    ASSERT_TRUE(res2.equals(conv(m2,m1)));
 }
 
 int main(int argc, char **argv)
